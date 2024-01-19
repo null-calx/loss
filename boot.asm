244f4d0e69d2e991bf59164b2af3ae8339396c24
calx <108835591+null-calx@users.noreply.github.com>
2024-01-19 17:38:30 +0530

    echo

--- boot.asm
+++ boot.asm
@@ -1,22 +1,11 @@
 	[org 0x7C00]
 
-	mov ah, 0x0E		; switch to teletype mode
-	mov bx, str
-
-loop:
-	mov al, [bx]
-	cmp al, 0
-	je exit
-
+loop:	
+	mov ah, 0
+	int 0x16
+	mov ah, 0x0E
 	int 0x10
-
-	inc bx
 	jmp loop
-
-exit:	
-	jmp $
-
-str:	db "Hello, world!", 0
 
 	times 510 - ($-$$) db 0
 	db 0x55, 0xAA
