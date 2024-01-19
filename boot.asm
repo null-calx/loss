366dc6efc58a43ee77582bc21c82ddce5f2d7f07
calx <108835591+null-calx@users.noreply.github.com>
2024-01-19 18:54:17 +0530

    print_string function

--- boot.asm
+++ boot.asm
@@ -1,11 +1,26 @@
 	[org 0x7C00]
 
-loop:	
-	mov ah, 0
-	int 0x16
+	mov bp, 0x8000
+	mov sp, bp
+
+	mov bx, str
+	call print_string
+
+	jmp $
+
+str:	db "Hello, world!", 0
+
+print_string:
 	mov ah, 0x0E
+print_loop:	
+	mov al, [bx]
+	cmp al, 0
+	je print_string_ret
 	int 0x10
-	jmp loop
+	inc bx
+	jmp print_loop
+print_string_ret:
+	ret
 
 	times 510 - ($-$$) db 0
 	db 0x55, 0xAA
