30b60f2e3d752cfb8b5b65ddea32c13f8eedfdb6
calx <108835591+null-calx@users.noreply.github.com>
2024-01-20 08:06:43 +0530

    Load the disk

--- boot.asm
+++ boot.asm
@@ -1,56 +1,42 @@
 	[org 0x7C00]
 
-	mov	bp, 0x8000
-	mov	sp, bp
+;;; int 0x13
 
-	mov	bx, str
-	call	print_string
-	mov	bx, newline
-	call	print_string
+;;; what disk we want to read?
+	mov	[disk_num], dl
 
-	mov	si, 0xFFFF
-number_loop:
-	inc	si
-	call	print_number
-	mov	bx, newline
-	call	print_string
-	cmp	si, 0xFFFF
-	jne	number_loop
+;;; CHS address? (cylinder, head, sector)
+;;; C = 0, H = 0, S = 2 (S starts with 1)
 
-	jmp	$
+;;; how many sectors?
+;;; 1
 
-str:	db "Hello, world!", 0
-newline:db 10, 13, 0
-space:	db ' ', 0
-print_number_buffer:
-	times 6 db 0
+;;; where to load them?
+;;; 0x7E00
 
-;;; si = byte
-print_number:
-	mov	bx, print_number_buffer + 5
-	mov	ax, si
-	mov	cx, 10
-print_number_loop:
-	dec	bx
-	xor	dx, dx
-	div	cx
-	add	dl, '0'
-	mov	[bx], dl
-	cmp	ax, 0
-	jne	print_number_loop
+	mov	ah, 2
+	mov	al, 1		; number of sectors
+	mov	ch, 0		; C
+	mov	cl, 2		; S
+	mov	dh, 0		; H
+	mov	dl, [disk_num]	; drive number
+	;; es:bx = 0x7E00
+	;; es * 16 + bx = 0x7E00
+	mov	bx, 0
+	mov	es, bx
+	mov	bx, 0x7E00
+	int	0x13
 
-;;; bx = *cstring
-print_string:
 	mov	ah, 0x0E
-print_string_loop:	
-	mov	al, [bx]
-	cmp	al, 0
-	je	print_string_exit
+	mov	al, [0x7E00]
 	int	0x10
-	inc	bx
-	jmp	print_string_loop
-print_string_exit:
-	ret
+
+	jmp $
+
+disk_num:
+	db 0
 
 	times 510 - ($-$$) db 0
 	db 0x55, 0xAA
+
+	times 512 db 'A'
